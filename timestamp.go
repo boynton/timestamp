ba44f152fea1fa3084c695c9b05525003b87163b
Lee R. Boynton <lee@boynton.com>
2015-02-01 16:25:58 -0800

    Added filename mode

--- timestamp.go
+++ timestamp.go
@@ -1,16 +1,31 @@
 package main
 
-import "fmt"
-import "time"
+import (
+	"fmt"
+	"os"
+	"time"
+)
 
 func main() {
 	t := time.Now().UTC()
-	fmt.Printf("%d-%02d-%02dT%02d:%02d:%02d.%03dZ\n",
-		t.Year(),
-		t.Month(),
-		t.Day(),
-		t.Hour(),
-		t.Minute(),
-		t.Second(),
-		t.Nanosecond()/1000000)
+	var format string
+	switch len(os.Args) {
+	case 1:
+		format = "%d-%02d-%02dT%02d:%02d:%02d.%03dZ\n"
+	case 2:
+		if os.Args[1] != "-f" {
+			usage()
+		}
+		format = "%d%02d%02dT%02d%02d%02d%03d"
+	default:
+		usage()
+	}
+	fmt.Printf(format, t.Year(), t.Month(), t.Day(), t.Hour(), t.Minute(), t.Second(), t.Nanosecond()/1000000)
+}
+
+func usage() {
+	fmt.Printf("usage:\n")
+	fmt.Printf("    timestamp        show the current time in RFC3339 format, UTC, with millisecond resolution, and trailing newline\n")
+	fmt.Printf("    timestamp -f     show the current time in a filename friendly format, with no newline\n")
+	os.Exit(1)
 }
